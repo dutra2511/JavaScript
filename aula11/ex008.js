77a5902ee36846474290ba2fc11fe55b987afdea
dutra2511 <marcelodutra_sensei@hotmail.com>
2024-08-13 17:58:07 -0300

    13/08

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A sua velocidade é de ${vel}Km/h.`)
+if (vel > 60){
+    console.log('Você ultrapaçou a velocidade permitida, você foi MULTADO!')
+}
+console.log('Dirija sempre com Cinto de Segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Argentina'
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é Estrangeiro!')
+}
