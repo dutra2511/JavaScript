ba0b1f231382270dfb4122ee00ba69f3e6c65224
dutra2511 <marcelodutra_sensei@hotmail.com>
2024-08-20 11:25:58 -0300

    20/08 02

--- aula15/vector.js
+++ aula15/vector.js
@@ -4,6 +4,6 @@ for(pos = 0;pos < valores.length; pos++){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }*/
 for(let pos in valores){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    console.log (`A posição ${pos} tem o valor ${valores[pos]}`)
 }
 console.log(valores.indexOf(7))
---
+++ aula16/função01.js
@@ -0,0 +1,8 @@
+function parimp(n){
+    if (n % 2 == 0){
+        return console.log('Par')
+    }else{
+        return console.log('Ímpar')
+    }
+}
+parimp(11)
