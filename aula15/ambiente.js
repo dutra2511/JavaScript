1841f7b76bfa9ee87ae2cdb96533d5e084996326
dutra2511 <marcelodutra_sensei@hotmail.com>
2024-08-20 10:48:07 -0300

    20/08 01

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+console.log(`Nossos valores são ${num}`)
+console.log(num[2])
+console.log(`O vector tem ${num.length} posições`)
+console.log(`o vector em ordem crescente ${num.sort()}`)
+console.log(num.push(1))
+console.log(`Nossos valores são ${num}`)
